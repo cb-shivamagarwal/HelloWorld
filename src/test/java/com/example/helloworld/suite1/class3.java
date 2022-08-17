339fc9944e5e945c3568e4e3c2b32eef449a2bf3
cb-shivam <98250333+cb-shivam@users.noreply.github.com>
2022-08-17 23:56:19 +0530

    Added TestNg Tests

---
+++ src/test/java/com/example/helloworld/suite1/class3.java
@@ -0,0 +1,17 @@
+package com.example.helloworld.suite1;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class class3 {
+    @Test
+    public void test1() {
+        assertTrue(2 % 2 == 0);
+    }
+
+    @Test
+    public void test2() {
+        assertTrue(2 % 2 == 0);
+    }
+}
